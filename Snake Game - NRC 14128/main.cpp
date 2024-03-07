365c0b355ff593956187465fe29c1c7c049fe020
ErickAriel29 <162519111+ErickAriel29@users.noreply.github.com>
2024-03-07 18:48:46 -0500

    Quitar y añadir funciones main.cpp

--- Snake Game - NRC 14128/main.cpp
+++ Snake Game - NRC 14128/main.cpp
@@ -23,68 +23,44 @@ bool colisionConSerpiente(const Punto& cabeza, const vector<Punto>& serpiente) {
 bool colisionConBorde(const Punto& cabeza, const RenderWindow& ventana) {
     return static_cast<unsigned int>(cabeza.x) < 0 || static_cast<unsigned int>(cabeza.y) < 0 || static_cast<unsigned int>(cabeza.x) >= ventana.getSize().x / 20 || static_cast<unsigned int>(cabeza.y) >= ventana.getSize().y / 20;
 }
-int main() {
-// Carga los sonidos
-    SoundBuffer bufferComer;
+// Función para cargar los sonidos
+void cargarSonidos(SoundBuffer& bufferComer, SoundBuffer& bufferColision, SoundBuffer& bufferHito, Sound& sonidoComer, Sound& sonidoColision, Sound& sonidoHito) {
     if (!bufferComer.loadFromFile("comer.wav")) {
         cout << "Error al cargar el archivo de sonido 'comer.wav'" << endl;
-        return -1;
+        exit(-1);
     }
-    Sound sonidoComer;
     sonidoComer.setBuffer(bufferComer);
 
-    SoundBuffer bufferColision;
     if (!bufferColision.loadFromFile("colision.wav")) {
         cout << "Error al cargar el archivo de sonido 'colision.wav'" << endl;
-        return -1;
+        exit(-1);
     }
-    Sound sonidoColision;
     sonidoColision.setBuffer(bufferColision);
 
-    SoundBuffer bufferHito;
     if (!bufferHito.loadFromFile("logro.wav")) {
         cout << "Error al cargar el archivo de sonido 'logro.wav'" << endl;
-        return -1;
+        exit(-1);
     }
-    Sound sonidoHito;
     sonidoHito.setBuffer(bufferHito);
+}
 
-    // Carga la música de fondo
-    
-    Music musicaFondo;
+// Función para cargar la música de fondo
+void cargarMusica(Music& musicaFondo) {
     if (!musicaFondo.openFromFile("musica.wav")) {
         cout << "Error al cargar el archivo de música 'musica.wav'" << endl;
-        return -1;
+        exit(-1);
     }
     musicaFondo.setLoop(true); // hace que la música se repita
     musicaFondo.play();
-    vector<Punto> serpiente;
-    serpiente.push_back({rand() % 40, rand() % 30}); // Posición inicial aleatoria de la serpiente
-   
-    vector<Punto> comidas;
-    Punto comida;
-    bool comidaEnSerpiente;
-    do {
-    comidaEnSerpiente = false;
-    comida = {rand() % 40, rand() % 30}; // Posición inicial aleatoria de la comida
-    for (Punto p : serpiente) {
-        if (p.x == comida.x && p.y == comida.y) {
-            comidaEnSerpiente = true;
-            break;
-        }
-    }
-} while (comidaEnSerpiente);
-comidas.push_back(comida);
+}
 
-Punto dir = {1, 0};
-
-    int puntuacion = 0;
-    int logros = 0;
-    int velocidad = 100;
-
-    // Carga la fuente
-    Font fuente;
-    fuente.loadFromFile("Montserrat-Black.ttf");
+// Función para crear la ventana en modo de pantalla completa
+void crearVentana(RenderWindow& ventana, RectangleShape& pixel) {
+    ventana.create(VideoMode::getDesktopMode(), "Juego de Serpiente", Style::Fullscreen);
+    pixel.setSize(Vector2f(20, 20));
+    pixel.setOutlineThickness(1);
+    pixel.setOutlineColor(Color::Black);
+}
 
 
 

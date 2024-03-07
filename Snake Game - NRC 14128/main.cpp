b2dca44c7f008e10760f43023bd24a90e6e96fda
ErickAriel29 <162519111+ErickAriel29@users.noreply.github.com>
2024-03-07 16:48:56 -0500

    Añadir 1 main.cpp

--- Snake Game - NRC 14128/main.cpp
+++ Snake Game - NRC 14128/main.cpp
@@ -1,1 +1,21 @@
+//crear funcion de colision si la serpiente choca con esta misma 
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+struct Punto {
+    int x, y;
+};
+
+bool colisionConSerpiente(const Punto& cabeza, const vector<Punto>& serpiente) {
+    for (auto& parte : serpiente) {
+        if (parte.x == cabeza.x && parte.y == cabeza.y)
+            return true;
+    }
+    return false;
+}
 

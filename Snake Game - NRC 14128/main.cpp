ffd6b87ee6e4ef6720b2402e56a87997324d481c
mrcolinaa <160189543+mrcolinaa@users.noreply.github.com>
2024-03-07 20:41:38 -0500

    git add . git commit -m "Añadidas las funciones: crearTextoPuntuacion, crearTextoVelocidad, crearBotones, crearBotonesDificultad, crearTextoInicio e iniciarJuego"

--- Snake Game - NRC 14128/main.cpp
+++ Snake Game - NRC 14128/main.cpp
@@ -86,113 +86,109 @@ void cargarFuente(Font& fuente) {
     fuente.loadFromFile("Montserrat-Black.ttf");
 }
 
-// Función para crear el texto de la puntuación
+
 void crearTextoPuntuacion(Text& textoPuntuacion, Font& fuente) {
-    textoPuntuacion.setFont(fuente);
-    textoPuntuacion.setCharacterSize(24);
-    textoPuntuacion.setFillColor(Color::White);
-    textoPuntuacion.setPosition(10, 10);
+    textoPuntuacion.setCharacterSize(30);
+    textoPuntuacion.setFillColor(Color::Black);
+    textoPuntuacion.setPosition(100, 100);
+}
+
+void crearTextoVelocidad(Text& textoVelocidad, Font& fuente) {
+    textoVelocidad.setCharacterSize(36);
+    textoVelocidad.setFillColor(Color::Red);
+    textoVelocidad.setPosition(200, 200);
+}
+
+void crearBotones(Text& botonJugar, Text& botonCerrar, Text& botonReintentar, Text& botonFinalizar, Font& fuente, RenderWindow& ventana) {
+    botonJugar.setFont(fuente);
+    botonJugar.setCharacterSize(42);
+    botonJugar.setFillColor(Color::Blue);
+    botonJugar.setPosition(ventana.getSize().x / 2 - botonJugar.getGlobalBounds().width / 2 - 100, ventana.getSize().y / 2 + 250);
+
+    botonCerrar.setFont(fuente);
+    botonCerrar.setCharacterSize(36);
+    botonCerrar.setFillColor(Color::Green);
+    botonCerrar.setPosition(ventana.getSize().x / 2 - botonCerrar.getGlobalBounds().width / 2 + 150, ventana.getSize().y / 2 + 250);
+
+    botonReintentar.setFont(fuente);
+    botonReintentar.setCharacterSize(48);
+    botonReintentar.setFillColor(Color::Yellow);
+    botonReintentar.setPosition(ventana.getSize().x / 2 - botonReintentar.getGlobalBounds().width / 2 - 50, ventana.getSize().y / 2 + 200);
+
+    botonFinalizar.setFont(fuente);
+    botonFinalizar.setCharacterSize(48);
+    botonFinalizar.setFillColor(Color::Blue);
+    botonFinalizar.setPosition(ventana.getSize().x / 2 - botonFinalizar.getGlobalBounds().width / 2 + 100, ventana.getSize().y / 2 + 200);
+}
+
+void crearBotonesDificultad(Text& botonNormal, Text& botonDificil, Text& botonMuyDificil, Font& fuente, RenderWindow& ventana) {
+    botonNormal.setFont(fuente);
+    botonNormal.setString("Normal (Velocidad normal)");
+    botonNormal.setCharacterSize(48);
+    botonNormal.setFillColor(Color::Blue);
+    botonNormal.setPosition(ventana.getSize().x / 2 - botonNormal.getGlobalBounds().width / 2 - 200, ventana.getSize().y / 2 - 100);
+
+    botonDificil.setFont(fuente);
+    botonDificil.setString("Difícil (Velocidad rápida)");
+    botonDificil.setCharacterSize(45);
+    botonDificil.setFillColor(Color::Green);
+    botonDificil.setPosition(ventana.getSize().x / 2 - botonDificil.getGlobalBounds().width / 2, ventana.getSize().y / 2);
+
+    botonMuyDificil.setFont(fuente);
+    botonMuyDificil.setString("Muy difícil (Velocidad muy rápida)");
+    botonMuyDificil.setCharacterSize(50);
+    botonMuyDificil.setFillColor(Color::Yellow);
+    botonMuyDificil.setPosition(ventana.getSize().x / 2 - botonMuyDificil.getGlobalBounds().width / 2 + 100, ventana.getSize().y / 2 + 100);
+}
+
+void crearTextoInicio(Text& textoInicio1, Text& textoInicio2, Font& fuente, RenderWindow& ventana) {
+    textoInicio1.setFont(fuente);
+    textoInicio1.setString("BIENVENIDO AL JUEGO DE LA SERPIENTE");
+    textoInicio1.setCharacterSize(60);
+    textoInicio1.setFillColor(Color::Blue);
+    textoInicio1.setPosition(ventana.getSize().x / 2 - textoInicio1.getGlobalBounds().width / 2, ventana.getSize().y / 9);
+    textoInicio2.setFont(fuente);
+    textoInicio2.setString("Proyecto de Final de Semestre\nDesarrollado por:\n\nJugador 1\nJugador 2\nJugador 3");
+    textoInicio2.setCharacterSize(48);
+    textoInicio2.setFillColor(Color::Green);
+    textoInicio2.setPosition(ventana.getSize().x / 2 - textoInicio2.getGlobalBounds().width / 2, ventana.getSize().y / 2.3);
+}
+
+
+void iniciarJuego(bool& juegoIniciado, bool& juegoTerminado, vector<Punto>& serpiente, vector<Punto>& comidas, Punto& comida, Punto& dir, int& puntuacion, int& logros) {
+
+    juegoIniciado = false;
+
+
+    juegoTerminado = false;
+
+
+    serpiente.clear();
+
+
+    serpiente.push_back({ 0, 0 });
+
+
+    comidas.clear();
+
+
+    comida = { 10, 10 };
+
+
+    comidas.push_back(comida);
+
+
+    dir = { 1, 1 };
+
+    puntuacion = 100;
+
+    logros = 5;
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Funcion principal
+// Función principal
 int main() {
     SoundBuffer bufferComer, bufferColision, bufferHito;
     Sound sonidoComer, sonidoColision, sonidoHito;
@@ -284,15 +280,15 @@ int main() {
         // Dibuja la pantalla de dificultad si el juego no ha iniciado
         Punto siguiente = {serpiente.back().x + dir.x, serpiente.back().y + dir.y};
         bool comio = false; // Indica si la serpiente comio una comida
-        for (int i = 0; i < comidas.size(); i++) { 
-            if (siguiente.x == comidas[i].x && siguiente.y == comidas[i].y) { 
+        for (int i = 0; i < comidas.size(); i++) {
+            if (siguiente.x == comidas[i].x && siguiente.y == comidas[i].y) {
                 sonidoComer.play(); // Reproduce el sonido de comer
                 do { // Comprueba si la comida se genera dentro de la serpiente y la vuelve a generar si es asi
-                    comidaEnSerpiente = false; 
+                    comidaEnSerpiente = false;
                     comida = {rand() % 40, rand() % 30}; // Nueva posicion aleatoria de la comida
-                    for (Punto p : serpiente) { 
-                        if (p.x == comida.x && p.y == comida.y) { 
-                            comidaEnSerpiente = true; 
+                    for (Punto p : serpiente) {
+                        if (p.x == comida.x && p.y == comida.y) {
+                            comidaEnSerpiente = true;
                             break;
                         }
                     }
